2fa01a3f527e84c97a7186f9e4a5005226ceeb49
0hDEADBEAF <0xdeadbeaf@tutamail.com>
2025-09-05 21:26:27 +0200

    Added btop (closes #15)

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -100,6 +100,12 @@ yay -Sy \
     uv \
     ruff
 
+# Installing rocm-smi-lib only if the computer uses a AMD GPU (so it can be displayed in btop)
+is_amd_gpu=$(lspci -k | grep -A 2 -E '(VGA|3D)' | grep 'Kernel driver in use: amdgpu')
+if [ -n "$is_amd_gpu" ]; then
+    yay -Sy rocm-smi-lib
+fi
+
 # Installing ty (Astral LS)
 uv tool install ty@latest
 uv tool update-shell
