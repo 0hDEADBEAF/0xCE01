8c91874fff3fd2a9e993029dd9515ba708f9e623
0hDEADBEAF <0xdeadbeaf@tutamail.com>
2025-09-08 23:54:25 +0200

    Added few Neovim keybindings (#16)

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -3,12 +3,19 @@ local opts = {
     silent = true,
 }
 
--- Unmapping conflicting keybindings
--- vim.keymap.del("i", "<C-l>");
+-- AZERTY fixes
+vim.keymap.set("n", "z", "w", opts) -- Move to next word
+vim.keymap.set("n", "q", "a", opts) -- Enter insert mode after the cursor
 
-
--- Settings Insert Mode keybindings
+-- Some nice bindings
 vim.keymap.set("n", "<C-t>", ">>", opts) -- Adds a level of tabulation to the current line
 vim.keymap.set("n", "<C-r>", "<<", opts) -- Removes a level of tabulation to the current line
 vim.keymap.set("n", "<C-j>", "yyp", opts) -- Copies the current line down
 vim.keymap.set("n", "<C-d>", "dd", opts) -- Removes the current line
+
+-- Neo-tree
+vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", opts) -- Toggle Neotree
+
+-- Terminal
+vim.keymap.set("n", "<C-b>", ":ToggleTerm<CR>", opts) -- Toggle terminal
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", opts) -- Exit terminal mode
